0e43ef803fcb73444ce458a900581d196b450043
Alex Skorulis <skorulis@gmail.com>
2024-06-23 12:33:23 +1000

    Fetch a journal month in notion

--- HomeServer/Sources/App/Controllers/NotionController.swift
+++ HomeServer/Sources/App/Controllers/NotionController.swift
@@ -17,7 +17,24 @@ struct NotionController: RouteCollection {
     
     @preconcurrency
     private func fetch(req: Request) async throws -> String {
-        "Test"
+        let response = try await req.client.get(
+            "https://api.notion.com/v1/blocks/90070adc-6caf-4604-a31a-f2b7bcbd3bdd/children?page_size=100",
+            headers: headers
+        )
+        if var body = response.body {
+            var result = ""
+            while body.readableBytes > 0 {
+                result += body.readString(length: body.readableBytes) ?? ""
+            }
+            return result
+        }
+        return "ERROR"
+    }
+    
+    private var headers: HTTPHeaders {
+        let authHeader = ("Authorization", "Bearer \(secrets.notionKey)")
+        let versionHeader = ("Notion-Version", "2022-06-28")
+        return .init([authHeader, versionHeader])
     }
 }
 
